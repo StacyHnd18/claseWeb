2d8d9cc85930448b615ab6881b5bcf72b94ea443
StacyHnd18 <79615106+StacyHnd18@users.noreply.github.com>
2021-03-04 15:24:08 -0600

    Add files via upload

---
+++ revertir2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+int main() {
+   char cadena[]= " ";
+	
+	cin >> cadena;
+	
+	strrev(cadena); 
+	
+	cout <<cadena<<endl ;
+	
+	returnd 0 ;
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
