cf5a0c06a9cfa9cd011565cdba43cd99178da5bc
StacyHnd18 <79615106+StacyHnd18@users.noreply.github.com>
2021-03-04 15:25:24 -0600

    Add files via upload

---
+++ polindromo.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	
+	string texto = " ";
+	
+	cin >> texto ;
+	
+	string reversa = texto;
+	
+	reverse(reverse.begin(), reversa.end());
+	
+	if (texto == reversa) 
+	
+	cout << " la cadena es un polindromo " ;
+	
+	
+	else
+	
+	cout << "la cadena es un polindromo " ;
+	
+	
+	
+	returnd;
+	}
