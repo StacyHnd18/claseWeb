3c44ce04db8beb5df3e0c24daf32d5c5bb3547ab
StacyHnd18 <79615106+StacyHnd18@users.noreply.github.com>
2021-03-04 15:26:12 -0600

    Add files via upload

---
+++ fechas.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	
+	string dia;
+	strig mes;
+	string año;
+	string fecha;
+	cout << "ingrese el dia ",
+	cin >> dia ;
+	cout << "ingrese mes ",
+	cin >> mes ;
+	cout << "ingrese el año ",
+	
+	
+	
+	
+	cout<< dia+ "/" +mes+"/"+año ;
+
+
+  
+       returnd ;
+
+	
+	}
